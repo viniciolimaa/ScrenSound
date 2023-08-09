f86c1bb25069ec3e361f6f92666ad9732a8a279d
Marcos Vinicio Lima <107953683+viniciolimaa@users.noreply.github.com>
2023-08-08 22:58:33 -0300

    Add files via upload

---
+++ ScreenSound03-projeto-inicial/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public int Nota { get; }
+    public Avaliacao(int nota) { 
+    
+        if(nota <0 || nota > 10)
+        {
+        Console.WriteLine("nota invalida");
+        }
+        else { Nota = nota; }
+    }
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
